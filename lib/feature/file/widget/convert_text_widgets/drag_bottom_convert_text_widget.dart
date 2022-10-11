ea7411a1ae59e504ed1985da4774680104bd6fba
LeThanh2312 <leduythanh2312@gmail.com>
2022-10-11 23:33:42 +0700

    update bottom sheet convert text

--- lib/feature/file/widget/convert_text_widgets/drag_bottom_convert_text_widget.dart
+++ lib/feature/file/widget/convert_text_widgets/drag_bottom_convert_text_widget.dart
@@ -19,92 +19,98 @@ class _DragBottomConvertTextWidgetState
 
   @override
   Widget build(BuildContext context) {
-    print('===== show text =====');
+    print('===== show text ===== ${widget.listString}');
     return DraggableScrollableSheet(
       initialChildSize: 0.30,
       minChildSize: 0.15,
       maxChildSize: 0.85,
       builder: (BuildContext context, ScrollController scrollController) {
-        return SingleChildScrollView(
-          controller: scrollController,
-          child: Card(
-            elevation: 12.0,
-            shape: const RoundedRectangleBorder(
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(15),
-                topRight: Radius.circular(15),
-              ),
-            ),
-            margin: const EdgeInsets.all(0),
-            child: Stack(
-              children: [
-                // SizedBox(
-                //         height: MediaQuery.of(context).size.height * 0.79,
-                //         child: SingleChildScrollView(
-                //           child: widget.listString != null
-                //               ? ListView.builder(
-                //                   shrinkWrap: true,
-                //                   physics: const BouncingScrollPhysics(),
-                //                   itemCount: widget.listString!.length,
-                //                   itemBuilder: (context, index) =>
-                //                       Text(widget.listString![index]),
-                //                 )
-                //               : Container(),
-                //         ),
-                //       ),
-                Container(
-                  margin: const EdgeInsets.only(top: 20, left: 10, right: 10),
-                  child: widget.listString != null
-                      ? SingleChildScrollView(
-                          child: ListView.builder(
-                              shrinkWrap: true,
-                              physics: const BouncingScrollPhysics(),
-                              itemCount: widget.listString!.length,
-                              itemBuilder: (context, index) {
-                                TextEditingController textController =
-                                    TextEditingController();
-                                textController.text = widget.listString![index];
-                                return TextField(
-                                  autofocus: false,
-                                  keyboardType: TextInputType.multiline,
-                                  style: const TextStyle(
-                                    decoration: TextDecoration.none,
-                                  ),
-                                  controller: textController,
-                                );
-                              }),
-                        )
-                      : Container(
-                          height: MediaQuery.of(context).size.height * 0.79,
-                          margin: const EdgeInsets.only(
-                              top: 20, left: 10, right: 10),
-                          child: const Center(
-                            child: Text(
-                              'Không nhận dạng được văn bản',
-                              style: TextStyle(color: Colors.black),
+        return Container(
+          height: MediaQuery.of(context).size.height * 0.9,
+          width: MediaQuery.of(context).size.width,
+          decoration: const BoxDecoration(
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(20), topRight: Radius.circular(20)),
+            color: Colors.white,
+          ),
+          child: Stack(
+            children: [
+              (widget.listString != null && widget.listString!.isNotEmpty)
+                  ? Container(
+                      height: MediaQuery.of(context).size.height * 0.9,
+                      width: MediaQuery.of(context).size.width,
+                      padding:
+                          const EdgeInsets.only(top: 0, left: 10, right: 10),
+                      child: SingleChildScrollView(
+                        controller: scrollController,
+                        child: Column(
+                          children: [
+                            const SizedBox(
+                              height: 50,
                             ),
-                          ),
+                            ...List.generate(
+                                widget.listString!.length,
+                                (index) => Container(
+                                    height: 50,
+                                    child: Text(widget.listString![index])))
+                            // ListView.builder(
+                            //     shrinkWrap: true,
+                            //     physics: const BouncingScrollPhysics(),
+                            //     itemCount: widget.listString!.length,
+                            //     itemBuilder: (context, index) {
+                            //       TextEditingController textController =
+                            //           TextEditingController();
+                            //       textController.text =
+                            //           widget.listString![index];
+                            //       return TextField(
+                            //         autofocus: false,
+                            //         keyboardType: TextInputType.multiline,
+                            //         style: const TextStyle(
+                            //           decoration: TextDecoration.none,
+                            //         ),
+                            //         controller: textController,
+                            //       );
+                            //     }),
+                          ],
                         ),
-                ),
-                Positioned(
-                  top: 0,
-                  left: 0,
-                  right: 0,
-                  child: Container(
-                    decoration: const BoxDecoration(
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(15),
-                        topRight: Radius.circular(15),
                       ),
-                      color: Colors.grey,
+                    )
+                  : Container(
+                      height: MediaQuery.of(context).size.height * 0.9,
+                      width: MediaQuery.of(context).size.width,
+                      margin:
+                          const EdgeInsets.only(top: 20, left: 10, right: 10),
+                      child: const Center(
+                        child: Text(
+                          'Không nhận dạng được văn bản',
+                          style: TextStyle(color: Colors.black),
+                        ),
+                      ),
                     ),
-                    child: Center(
-                      child: CustomDraggingHeader(),
-                    ),
+              IgnorePointer(
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: const BorderRadius.only(
+                        topLeft: Radius.circular(20),
+                        topRight: Radius.circular(20)),
+                    color: Colors.grey[200],
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.only(top: 10, bottom: 10),
+                        height: 10,
+                        width: 100,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.grey),
+                      ),
+                    ],
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         );
       },
